d727586f2d4331b051f7213a3a3d898f7a534f23
MaruthiVsm16 <86735262+Maruthivsm16@users.noreply.github.com>
2025-09-03 14:07:41 +0000

    testing second time

--- git/git_auto.sh
+++ git/git_auto.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 commit_msg=${1:-"auto commit $timestamp"}
+branch=$(git branch --show-current)
 echo "staging all changes"
 git add -A 
 echo "Committing with message: $commit_msg"
 git commit -m "$commit_msg"
 echo "pushing to remote"
-git push origin main
+git push origin "$branch"
 echo " Done!"
